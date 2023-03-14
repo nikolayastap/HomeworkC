44a3b397935cf938f048a01ac191a407440b0162
Astashenko Nikolay <astashenko3@gmail.com>
2023-03-14 03:15:44 +0300

    done task2

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int m = int.Parse(Console.ReadLine()!);
+if (n > m)
+    Console.WriteLine($"max = {n}, min = {m}");
+else if (n < m)
+    Console.WriteLine($"max = {m}, min = {n}");
+else
+    Console.WriteLine("=");

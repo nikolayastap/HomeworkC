aff04e339f7a31c5f26d4d597d1274bd0350f004
Astashenko Nikolay <astashenko3@gmail.com>
2023-03-14 19:40:55 +0300

    done task4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3-ое число: ");
+int c = int.Parse(Console.ReadLine()!);
+int Max = a;
+if (Max < b)
+      Console.WriteLine(b);
+else if (Max < c)
+    Console.WriteLine(c);
+else
+    Console.WriteLine(a);

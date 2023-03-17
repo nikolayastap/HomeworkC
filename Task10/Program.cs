816309a51fa051458041dcf01632e15e466d2f8e
Astashenko Nikolay <astashenko3@gmail.com>
2023-03-18 02:29:45 +0300

    done task10

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите трухзначенное число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 100 || n > 999)
+  {
+    Console.Write("Вы ошиблись! \n Введите трехзначенное число: ");
+    n = int.Parse(Console.ReadLine()!);
+  }
+Console.WriteLine(n / 10 % 10);

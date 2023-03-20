75d60445ab82866781b6e8ef2c38e94d34136546
Astashenko Nikolay <astashenko3@gmail.com>
2023-03-20 17:26:37 +0300

    done task15

---
+++ Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите день недели: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 1 || n > 7)
+{
+  Console.Write("Вы ошиблись! \nВведите день недели: ");
+  n = int.Parse(Console.ReadLine()!);
+}
+if (n > 5)
+  Console.WriteLine("Да");
+else
+  Console.WriteLine("Нет");

9dc8f13ef3f681dec3f9f1700d6851fd95eb1afc
Astashenko Nikolay <astashenko3@gmail.com>
2023-03-20 19:09:25 +0300

    done task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,7 +2,8 @@
 Console.Write("Введи число: ");
 int anyNumber = Convert.ToInt32(Console.ReadLine());
 string anyNumberText = Convert.ToString(anyNumber);
-if (anyNumberText.Length > 2){
+if (anyNumberText.Length > 2)
+{
   Console.WriteLine("третья цифра -> " + anyNumberText[2]);
 }
 else {

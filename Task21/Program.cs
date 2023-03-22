5dbacd1753d9d61d4fdb74d0fb62dd0161b9af5e
Astashenko Nikolay <astashenko3@gmail.com>
2023-03-23 00:23:05 +0300

    done task21

---
+++ Task21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите координату x(1): ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y(1): ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z(1): ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату x(2): ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y(2): ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z(2): ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double s = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+Console.WriteLine($"Растояние между двумя точками равно {Math.Round(s, 2)} ");

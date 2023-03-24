72d730933874d8811aa1d0e655394d3e8fdd8a35
Astashenko Nikolay <astashenko3@gmail.com>
2023-03-25 00:18:27 +0300

    done task19

---
+++ Task19/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string anyNumberText = Console.ReadLine()!;
+if (anyNumberText[0] == anyNumberText[4] && anyNumberText[1] == anyNumberText[3])
+{
+  Console.WriteLine("Да");
+}
+else {
+  Console.WriteLine("Нет");
+}
